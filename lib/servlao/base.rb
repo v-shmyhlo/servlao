4577946ccbc08f834affe7cb265691dd438d0bc7
v-shmyhlo <aion.stu@gmail.com>
2015-09-03 19:40:34 +0300

    lib

--- lib/servlao/base.rb
+++ lib/servlao/base.rb
@@ -1,7 +1,9 @@
 require 'servlao/base/config'
+require 'servlao/base/request'
 
 module Servlao
   module Base
     include Base::Config
+    include Base::Request
   end
 end
---
+++ lib/servlao/base/request.rb
@@ -0,0 +1,20 @@
+module Servlao
+  module Base
+    module Request
+      %i(get, post, put, patch, delete).each do |m|
+        define_method m do |uri, **opts, &block|
+          opts[:method] = m
+          request_for(uri, opts, &block)
+        end
+      end
+
+      def request_for(uri, **opts)
+        Typhoeus::Request.new(base_uri + uri, opts).tap do |request|
+          if block_given?
+            request.on_complete { |response| yield Yajl.load(response.body) }
+          end
+        end
+      end
+    end
+  end
+end
